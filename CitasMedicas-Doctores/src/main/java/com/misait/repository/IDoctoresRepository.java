82427bae7b66addde33d2f50808e92f18106ef66
Jorge Cuellar <159957497+jicuellar@users.noreply.github.com>
2024-03-20 12:02:09 -0600

    Delete CitasMedicas-Doctores directory

--- CitasMedicas-Doctores/src/main/java/com/misait/repository/IDoctoresRepository.java
+++
@@ -1,7 +0,0 @@
-package com.misait.repository;
-
-import com.misait.models.Doctores;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDoctoresRepository extends JpaRepository<Doctores,Long> {
-}
